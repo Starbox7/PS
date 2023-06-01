96978bc18a343275cbc309448c246185688640de
Starbox <39715563+Starbox7@users.noreply.github.com>
2023-06-01 15:19:30 +0900

    Jun. 01. 2023, Thu

---
+++ Programmers/Lv.0/배열의_유사도.py
@@ -0,0 +1,13 @@
+def solution(s1, s2):
+    answer = 0
+
+    for i in s1:
+        for j in s2:
+            if i == j :
+                answer += 1
+
+    return answer
+
+
+# def solution(s1, s2):
+#     return len(set(s1)&set(s2));
